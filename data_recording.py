8e7da5930bc89e6f04fde1a9a2f942b495db1756
Hendrik Borras <hendrikborras@web.de>
2017-08-07 11:24:15 +0200

    Support for simulated detector and changed GPS-class

--- data_recording.py
+++ data_recording.py
@@ -2,22 +2,21 @@ import sys
 sys.path.append('.')
 import time
 import sensors
+import detectors
 
-
-IMU_SETTINGS_FILE = "IMU_settings"
 
 # set up the IMU
+IMU_SETTINGS_FILE = "IMU_settings"
 imu = sensors.IMU_Reader(IMU_SETTINGS_FILE)
 
 # set up and start the GPS
-gps = sensors.GPS_location_provider();
-gps.start()
+gps = sensors.GPS_location_provider()
 
 
-do_test_run = True
-while do_test_run:
-    # print some data regularily
-    time.sleep(0.5)
+# def a recieving functino
+def do_something_with_new_detector_data(*args):
+    print("Fresh new data now at the detector!")
+    print(args)
     # print the last GPS data
     gps_data = gps.get_last_location_data()
     print("--> GPS: ", gps_data)
@@ -26,4 +25,8 @@ while do_test_run:
     # print imu data
     imu.print_IMU_and_pressure_data(IMU_data)
 
+# start our detector
+detec = detectors.simulated_detector()
+# subscribe to detector events
+detec.on_publish_new_data += do_something_with_new_detector_data
 
